676f2aba564500f2e9b07fc298abd85f89f55bec
nidhi goswami <nidhi29goswami@gmail.com>
2020-08-06 16:03:27 +0530

    module4 solution

---
+++ module4-solution/js/Speakhello.js
@@ -0,0 +1,9 @@
+(function(window){
+var helloSpeaker={};
+
+var speakWord="hello";
+helloSpeaker.speak=function(names){
+	console.log(speakWord +" "+ names);
+}
+window.helloSpeaker=helloSpeaker;
+})(window);
